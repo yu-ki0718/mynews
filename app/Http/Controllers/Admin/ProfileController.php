40ce27b5e974b1ed93cebf0f0a5ae4487fd2091d
岸波　雄樹 <you_work_24@icloud.com>
2019-06-18 22:14:20 +0800

    add view

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,13 +10,7 @@ class ProfileController extends Controller
     public function add(){
         return view('admin.profile.create');
     }
-    public function create(){
-        return view('admin/profile/create');
-    }
     public function edit(){
-        return view('adimin.profile.edit');
-    }
-    public function update(){
-        return view('admin/profile/update');
+        return view('admin.profile.edit');
     }
 }
