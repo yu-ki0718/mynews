4c70ab236cb21e2e4b8eeb9a448c334edf11ab51
岸波　雄樹 <you_work_24@icloud.com>
2019-06-19 23:02:41 +0800

    add user_authentication

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,14 @@
 */
 
 Route::group(['prefix'=>'admin'], function () {
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 });
 
 Route::group(['prefix'=>'admin'], function () {
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add') ->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
